7fff5102d66ab20fb13791f6021eb0a4b862caa7
takatoshiH <t.hikijiri@gmail.com>
2019-12-20 16:36:09 +0900

    一覧画面に機能を追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,6 @@
 User.create(email: 'admin@test.com', password: 'password') unless User.find_by(email: 'admin@test.com')
 User.create(email: 'satou@test.com', password: 'password') unless User.find_by(email: 'satou@test.com')
+
+Tweet.create(sentence: "いいかんじ")
+Tweet.create(sentence: "めっちゃいいかんじ")
+
