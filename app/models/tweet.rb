ab55d499eb818ca0965ebaa257da0d6126884278
takatoshiH <t.hikijiri@gmail.com>
2019-12-23 14:41:31 +0900

    投稿関連の修正

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,4 +3,8 @@ class Tweet < ApplicationRecord
   has_many :likes
   has_many :liked_users, through: :likes, source: :user
   has_many :replies
+
+  validates :sentence, presence: true
+  validates :sentence, length: { in: 1..140 }
+
 end
